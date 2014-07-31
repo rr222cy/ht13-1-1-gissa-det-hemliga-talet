a9d34de746e1d8d6797cbdc889715447fd5dbb80
rr222cy <rr222cy@student.lnu.se>
2014-07-31 15:16:21 +0200

    Class structure written

--- NumberGuessingGame/Models/SecretNumber.cs
+++ NumberGuessingGame/Models/SecretNumber.cs
@@ -7,5 +7,36 @@ namespace NumberGuessingGame.Models
 {
     public class SecretNumber
     {
+        private int? _number;
+        const int MaxNumberOfGuesses = 7;
+
+        public bool CanMakeGuess { get; }
+        public int Count { get; }
+        public IList<GuessedNumber> GuessedNumber { get; }
+        public GuessedNumber LastGuessedNumber { get; }
+        public int? Number { get; set; }
+        
+        public void Initialize()
+        {
+
+        }
+
+        public int MakeGuess(int guess)
+        {
+            return 0;
+        }
+
+        
+        public SecretNumber()
+        {
+            // EMPTY!
+        }
     }
+
+    public struct GuessedNumber
+    {
+        public int? Number;
+    }
+
+    enum Outcome { Indefinite, Low, High, Right, NoMoreGuesses, OldGuess }
 }
