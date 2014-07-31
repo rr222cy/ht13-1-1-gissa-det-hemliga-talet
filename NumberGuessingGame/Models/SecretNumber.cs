c0aedd41be139c98ae58e4b125f4064ae81e2760
rr222cy <rr222cy@student.lnu.se>
2014-07-31 15:40:45 +0200

    SecretNumber updates of properties

--- NumberGuessingGame/Models/SecretNumber.cs
+++ NumberGuessingGame/Models/SecretNumber.cs
@@ -5,15 +5,41 @@ using System.Web;
 
 namespace NumberGuessingGame.Models
 {
+    public enum Outcome { Indefinite, Low, High, Right, NoMoreGuesses, OldGuess }
+
     public class SecretNumber
     {
         private int? _number;
         const int MaxNumberOfGuesses = 7;
 
-        public bool CanMakeGuess { get; }
-        public int Count { get; }
-        public IList<GuessedNumber> GuessedNumber { get; }
-        public GuessedNumber LastGuessedNumber { get; }
+        public bool CanMakeGuess
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public IList<GuessedNumber> GuessedNumber 
+        {
+            get
+            {
+                throw new NotImplementedException();
+            } 
+        }
+        public GuessedNumber LastGuessedNumber 
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
         public int? Number { get; set; }
         
         public void Initialize()
@@ -36,7 +62,8 @@ namespace NumberGuessingGame.Models
     public struct GuessedNumber
     {
         public int? Number;
+        private Outcome Outcome;
     }
 
-    enum Outcome { Indefinite, Low, High, Right, NoMoreGuesses, OldGuess }
+    
 }
